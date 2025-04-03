6bcee8714044cddbda92104e8b5523548dc415a9
Amy <amiseye84@gmail.com>
2025-04-03 14:03:24 +0200

    mon projet Karate

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -2,17 +2,20 @@ package runner;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 
 import javax.xml.transform.Result;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class RunnerTest {
+
     @Test
-    public void testParallele(){
-        Results results = Runner.path("src/test/resources/features").parallel(1);
-        //assertTrue(results.getFailCount() == 0);
+    public void testParalleleTest(){
+        Results results = Runner.path("classpath:features").parallel(1);
+        //assertEquals(0, results.getFailCount());
     }
 }
